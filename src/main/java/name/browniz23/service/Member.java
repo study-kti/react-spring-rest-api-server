c242a5a3888aa1b8d384a56bf441074c8dd3169c
browniz421 <browniz421@gmail.com>
2019-05-15 19:36:40 +0900

    this가 동작하지 않아 getter, setter로 변경

--- src/main/java/name/browniz23/service/Member.java
+++ src/main/java/name/browniz23/service/Member.java
@@ -8,27 +8,41 @@ import lombok.*;
 @Table(name = "member")
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter(AccessLevel.PUBLIC)
-@Setter(AccessLevel.PRIVATE)
+@Setter(AccessLevel.PUBLIC)
 @EqualsAndHashCode
 public class Member {
 
     @Id
     @GeneratedValue
     private Long id;
-
     private String name;
-    private Integer age;
-
-//    @Enumerated(EnumType.STRING)
-//    private Grade grade;
-    
+	private Integer age;
+//  @Enumerated(EnumType.STRING)
+//  private Grade grade;
+  
     private Member() {}
 
     private Member(String name, Integer age) {
-        this.name = name;
-        this.age = age;
+        this.setName(name);
+        this.setAge(age);
 //        this.grade = grade;
     }
+
+    public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
 
     public static Member join(@NonNull String name, @NonNull Integer age) {
         return new Member(name, age);
