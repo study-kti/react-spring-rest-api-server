b8d963543f7f82129e188e8c3638b8ddc54f8047
browniz421 <browniz421@gmail.com>
2019-05-15 18:29:34 +0900

    init

---
+++ src/main/java/name/browniz23/ReactSpringRestApiServerApplication.java
@@ -0,0 +1,13 @@
+package name.browniz23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringRestApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringRestApiServerApplication.class, args);
+	}
+
+}
