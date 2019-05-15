131857b13b7f99eb183ece1c12154a130ab8a464
browniz421 <browniz421@gmail.com>
2019-05-15 20:07:33 +0900

    페이징 검색

--- src/main/java/name/browniz23/dao/MemberRepository.java
+++ src/main/java/name/browniz23/dao/MemberRepository.java
@@ -1,5 +1,6 @@
 package name.browniz23.dao;
 
+import org.springframework.data.domain.*;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.*;
 import org.springframework.data.rest.core.annotation.*;
@@ -9,5 +10,8 @@ import name.browniz23.service.*;
 @RepositoryRestResource
 public interface MemberRepository extends JpaRepository<Member, Long> {
 	
-	Member findByName(@Param("name") String name);
+//	검색
+//	Member findByName(@Param("name") String name);
+//	페이징 검색
+	Page<Member> findByName(@Param("name") String name, Pageable pageable);
 }
