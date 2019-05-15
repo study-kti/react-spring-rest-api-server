c39c3b3dbbfe3fb120dc4bd18713d4ba4d37b9a9
browniz421 <browniz421@gmail.com>
2019-05-15 19:49:15 +0900

    search API 추가

--- src/main/java/name/browniz23/dao/MemberRepository.java
+++ src/main/java/name/browniz23/dao/MemberRepository.java
@@ -1,10 +1,13 @@
 package name.browniz23.dao;
 
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.*;
 import org.springframework.data.rest.core.annotation.*;
 
 import name.browniz23.service.*;
 
 @RepositoryRestResource
 public interface MemberRepository extends JpaRepository<Member, Long> {
+	
+	Member findByName(@Param("name") String name);
 }
