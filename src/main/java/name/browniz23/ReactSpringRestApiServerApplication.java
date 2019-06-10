cd24109b60659032c61a2f3a9ffab29ae79741d6
정지훈 <browniz421@gmail.com>
2019-06-11 00:44:09 +0900

    no message

--- src/main/java/name/browniz23/ReactSpringRestApiServerApplication.java
+++ src/main/java/name/browniz23/ReactSpringRestApiServerApplication.java
@@ -10,7 +10,7 @@ public class ReactSpringRestApiServerApplication implements CommandLineRunner {
 	
 	@Autowired
 	public static void main(String[] args) {
-		sss
+	//
 		SpringApplication.run(ReactSpringRestApiServerApplication.class, args);
 	}
 	
